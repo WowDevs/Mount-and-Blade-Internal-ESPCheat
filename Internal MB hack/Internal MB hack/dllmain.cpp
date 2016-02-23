bd8648930643cab5303c3ab4bf4d10e4cf069555
FrancescoVentura <Franceso.Ventura34@gmail.com>
2016-02-23 01:10:04 +0100

    W2S WORKS!

--- Internal MB hack/Internal MB hack/dllmain.cpp
+++ Internal MB hack/Internal MB hack/dllmain.cpp
@@ -75,6 +75,7 @@ void RenderNames(LPDIRECT3DDEVICE9 pDevice)
 	}
 	else { return; }
 }
+
 void printMatrix(D3DXMATRIX matrix,float x,float y, D3DXMATRIX matrix2)
 {
 	if (a) {
@@ -91,10 +92,14 @@ void printMatrix(D3DXMATRIX matrix,float x,float y, D3DXMATRIX matrix2)
 		printf("\n %f , %f", x, y);
 		printf("\n");
 
-		printf("VcameraPos");
-		for (size_t t = 0; t < 4; t++)
+		printf("VcameraPos\n");
+		for (size_t i = 0; i < 4; i++)
 		{
-			printf("\t %f", matrix2.m[0][t]);
+			for (size_t t = 0; t < 4; t++)
+			{
+				cout << "\t" << matrix2.m[t][i];
+			}
+			printf("\n");
 		}
 
 		cout << "\n";
@@ -128,59 +133,56 @@ HRESULT WINAPI hkEndScene(LPDIRECT3DDEVICE9 pDevice)
 	return oEndScene(pDevice);
 }
 
-
-
 HRESULT WINAPI hkDIP(LPDIRECT3DDEVICE9 pDevice, D3DPRIMITIVETYPE Type, INT Base, UINT Min, UINT Num, UINT Start, UINT Prim)
 {
 	LPDIRECT3DVERTEXBUFFER9 streamData;
 	UINT offset = 0;
 	UINT stride = 0;
-	//Circle(pDevice, 150, 150, 1, 50, D3DCOLOR_RGBA(255, 125, 125, 0));
+
 	if (pDevice->GetStreamSource
 		(0, &streamData, &offset, &stride) == S_OK)
 		streamData->Release();
 	
 	if (!chams)
 	{
-		if (68 == stride)
+		if (68 == stride && number == 0)
 		{
-			/*pDevice->SetRenderState(D3DRS_ZENABLE, false);
+			pDevice->SetRenderState(D3DRS_ZENABLE, false);
 
 			for (size_t i = 0; i < 10; i++)
 			{
 				pDevice->SetTexture(i, red);
-			}*/
+			}
 
 			directx9Hack.doDisassembleShader(pDevice, "shader.txt");
 			D3DVIEWPORT9 Viewport;
 			D3DXMATRIX ViewProjectionMatrix, LocalToWorld, WorldToLocal;
 			D3DXVECTOR3 Vector3D(0, 0, 0), Vector2D;
-			pDevice->GetVertexShaderConstantF(159, ViewProjectionMatrix, 4);//change this to match your game matrix
-			pDevice->GetVertexShaderConstantF(174, LocalToWorld, 1);//change this to match your game matrix
+			pDevice->GetVertexShaderConstantF(159, ViewProjectionMatrix, 4);
+			pDevice->GetVertexShaderConstantF(174, LocalToWorld, 1);
 			pDevice->GetViewport(&Viewport);
 
 			D3DXMatrixIdentity(&WorldToLocal);
 			D3DXVec3Project(&Vector2D, &Vector3D, &Viewport, &ViewProjectionMatrix, &LocalToWorld, &WorldToLocal);
+
 			/*if (Vector2D.z < 1.0f)
 			{*/
-			ModelInfo_t pModelInfo = { static_cast<int>(Vector2D.x),static_cast<int>(Vector2D.y),0,"lol"};
+			ModelInfo_t pModelInfo = { static_cast<int>(Vector2D.x),static_cast<int>(Vector2D.y),0,""};
 			ModelInfo.push_back(pModelInfo);
-				/*x = Vector2D.x;
-				y = Vector2D.y;*/
-				//printMatrix(LocalToWorld, Vector2D.x, Vector2D.y, LocalToWorld);
+			printMatrix(ViewProjectionMatrix, Vector2D.x, Vector2D.y, LocalToWorld);
 
 				
 			/*}
 			*/
-			/*a = false;*/
-			/*number = 1;*/
-			/*pDevice->DrawIndexedPrimitive(Type, Base, Min, Num, Start, Prim);*/
+			a = false;
+			number = 1;
+			pDevice->DrawIndexedPrimitive(Type, Base, Min, Num, Start, Prim);
 		}
 
-	/*	if (68 == stride && number == 1)
+		if (68 == stride && number == 1)
 		{
 			number = 0;
-		}*/
+		}
 	}
 
 	return oDIP(pDevice, Type, Base, Min, Num, Start, Prim);
